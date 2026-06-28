e12e0b6fbe8216c602b847e150a38b80509e4ce1
willtryon <tryonwilliama@gmail.com>
2026-06-28 19:10:36 -0400

    recordRecord and recordScore sort by total top 10 (ie aren't stupid anymore)

--- src/main/java/com/willtryon/pokecard/CardImportsIndex.java
+++ src/main/java/com/willtryon/pokecard/CardImportsIndex.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.stream.Stream;
 
 import dev.brachtendorf.jimagehash.hash.Hash;
@@ -14,6 +16,7 @@ import org.bytedeco.opencv.opencv_features2d.ORB;
 
 public class CardImportsIndex {
     private final List<CardImports> imports = new ArrayList<>();
+    private record Scored(CardSignature sig, double score){}
 
     // hashed = DB entries with a non-null hash; index = source of the shared helpers
     public CardImportsIndex(Path compareDir, List<CardSignature> hashed, CardIndex cardDB) {
@@ -41,45 +44,51 @@ public class CardImportsIndex {
         File victim = new File(path.toString());
         try{
             Hash test = hasher.hash(victim);
+            PriorityQueue<Scored> topHash = new PriorityQueue<>(Comparator.comparingDouble((Scored s) -> s.score()).reversed());
             double record = Double.MAX_VALUE;
-            CardSignature recordHolder = null;
-            List<CardSignature> recordRecord = new ArrayList<>();
-            List<Double> recordScore = new ArrayList<>();
             for (int i = 0; i < hashed.size(); i++) {
                 double comp = test.normalizedHammingDistance(hashed.get(i).getBinaryHash());
-                if (comp < record) {
-                    recordRecord.add(hashed.get(i));
-                    recordScore.add(comp);
-                    record = comp;
-                    recordHolder = hashed.get(i);
-                }
+                topHash.offer(new Scored(hashed.get(i), comp));
+                if (topHash.size()>10) topHash.poll();
             }
-            System.out.println("\nUploaded image " + victim.toString() + " appears to be closest to " + recordHolder.getStringImgPath() + ". (pHash)");
-            System.out.println(record);
-            CardImports.Match hashMatch = new CardImports.Match(recordHolder.getCardID(), recordHolder.getStringImgPath(), record);
+            List <Scored> hashSorted = new ArrayList<>(topHash);
+            hashSorted.sort(Comparator.comparingDouble(Scored::score));
+            Scored bestHash = hashSorted.get(0);
+            System.out.println("\nUploaded image " + victim + " appears to be closest to " + bestHash.sig().getStringImgPath() + ". (pHash)");
+            System.out.println(bestHash.score());
+            CardImports.Match hashMatch = new CardImports.Match(bestHash.sig.getCardID(), bestHash.sig.getStringImgPath(), bestHash.score());
+            List<CardSignature> recordRecord = new ArrayList<>();
+            List<Double> recordScore = new ArrayList<>();
+            for(int s = 0; s<hashSorted.size();s++){
+                recordRecord.add(hashSorted.get(s).sig());
+                recordScore.add(hashSorted.get(s).score());
+            }
 
             // ---- ORB pass (higher is closer) ----
             ORB orb = ORB.create();
             CardIndex.Features test2 = cardDB.describe(path.toString(), orb);
+            PriorityQueue<Scored> bottomOrb = new PriorityQueue<>(Comparator.comparingDouble((Scored s)->s.score()));
             int record2 = 0;
-            CardSignature recordHolder2 = null;
             long startTime = System.currentTimeMillis();
-            List<CardSignature> recordRecord2 = new ArrayList<>();
-            List<Double> recordScore2 = new ArrayList<>();
             for (int i = 0; i < hashed.size(); i++) {
                 int comp = cardDB.geometricMatches(test2.desciptors, test2.keypoints, hashed.get(i).getMatData(), hashed.get(i).getKeypoints());
+                bottomOrb.offer(new Scored(hashed.get(i), comp));
+                if(bottomOrb.size()>10) bottomOrb.poll();
                 String percent = String.format("%.0f", ((double) i / hashed.size()) * 100);
                 System.out.println("\033[0F\033[K" + percent + "% " + cardDB.timer(startTime));
-                if (comp > record2) {
-                    record2 = comp;
-                    recordHolder2 = hashed.get(i);
-                    recordScore2.add((double)comp);
-                    recordRecord2.add(hashed.get(i));
-                }
             }
-            System.out.println("\nUploaded image " + victim.toString() + " appears to be closest to " + recordHolder2.getImgPath() + ". (ORB)");
-            System.out.println(record2);
-            CardImports.Match orbMatch = new CardImports.Match(recordHolder2.getCardID(), recordHolder2.getStringImgPath(), record2);
+            List<Scored> orbSorted = new ArrayList<>(bottomOrb);
+            orbSorted.sort(Comparator.comparingDouble(Scored::score));
+            Scored bestOrb = orbSorted.get(0);
+            System.out.println("\nUploaded image " + victim + " appears to be closest to " + bestOrb.sig.getStringImgPath() + ". (ORB)");
+            System.out.println(bestOrb.score());
+            CardImports.Match orbMatch = new CardImports.Match(bestOrb.sig.getCardID(), bestOrb.sig.getStringImgPath(), record2);
+            List<CardSignature> recordRecord2 = new ArrayList<>();
+            List<Double> recordScore2 = new ArrayList<>();
+            for(int s = 0; s<orbSorted.size(); s++){
+                recordRecord2.add(orbSorted.get(s).sig());
+                recordScore2.add(orbSorted.get(s).score());
+            }
             return new CardImports(path, hashMatch, orbMatch, recordScore, recordRecord, recordScore2, recordRecord2);
         } catch (IOException e) {
             System.out.println("File no worky :(");
